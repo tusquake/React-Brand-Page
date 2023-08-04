022eec87c9ec7fbbc7d48458812ee20834b8d107
Tushar Seth <77339749+tusquake@users.noreply.github.com>
2023-08-04 19:30:23 +0530

    Added

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+        
+      </div>
+      <ul>
+              <li><a href="#">Menu</a></li>
+              <li><a href="#">Location</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+      <button>login : Tushar</button>
+    </nav>
+  );
+};
+
+export default Navigation;
