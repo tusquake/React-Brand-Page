8e6ac0a6f737b17389010f173eedd72397c5b039
Tushar Seth <77339749+tusquake@users.noreply.github.com>
2023-10-01 20:12:13 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
         <p>
           YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
           SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-          SHOES.
+          SHOES COLLECTION.
         </p>
 
         <div className="hero-btn">
